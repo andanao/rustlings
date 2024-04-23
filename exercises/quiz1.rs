46b56387d27ffdd0f4642742f23b65c9818c0607
Adrian Danao-Schroeder <adriandanao@gmail.com>
2024-04-23 15:54:08 -0400

    quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,15 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apples_num:u32) -> u32 {
+    if apples_num <= 40 {
+	apples_num * 2
+    } else {
+	apples_num
+    }
+}
 
 // Don't modify this function!
 #[test]
