8e4b67f53d36938b329edc8cb0b4414308ed9360
Adrian Danao-Schroeder <adriandanao@gmail.com>
2024-05-07 13:48:34 -0400

    conversions

--- exercises/22_clippy/clippy3.rs
+++ exercises/22_clippy/clippy3.rs
@@ -1,9 +1,8 @@
 // clippy3.rs
-// 
+//
 // Here's a couple more easy Clippy fixes, so you can see its utility.
 // No hints.
 
-// I AM NOT DONE
 
 #[allow(unused_variables, unused_assignments)]
 fn main() {
@@ -24,7 +23,6 @@ fn main() {
     let mut value_a = 45;
     let mut value_b = 66;
     // Let's swap these two!
-    value_a = value_b;
-    value_b = value_a;
+    std::mem::swap(&mut value_a, &mut value_b);
     println!("value a: {}; value b: {}", value_a, value_b);
 }
--- exercises/23_conversions/from_str.rs
+++ exercises/23_conversions/from_str.rs
@@ -31,7 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -48,6 +47,22 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+	if s.is_empty() {
+	    Err(ParsePersonError::Empty)?
+	}
+	let mut parts = s.split(",");
+	let name = parts.next().unwrap().to_string();
+	if name.is_empty() {
+	    Err(ParsePersonError::NoName)?
+	};
+	let age: usize = parts.next().
+	    ok_or(ParsePersonError::BadLen)?.
+	    parse().map_err(ParsePersonError::ParseInt)?;
+	if let Some(x) = parts.next() {
+	    Err(ParsePersonError::BadLen)?
+	}
+	Ok(Person{name, age})
+
     }
 }
 
